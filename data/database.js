4816b307e15387c55510ef129dece369d3f4df92
harsh sharma <harshsharma@harshs-MacBook-Air-10.local>
2026-07-07 17:56:08 +0530

    prepare for deployment, did some changes in database.js file

--- data/database.js
+++ data/database.js
@@ -6,7 +6,6 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 3306,
-  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: true } : undefined,
 });
 
 module.exports = pool;
