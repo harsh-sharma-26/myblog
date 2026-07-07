801800e37164529f5c724e43e7eddb0d07591635
harsh sharma <harshsharma@harshs-MacBook-Air-10.local>
2026-07-07 18:14:18 +0530

    support railway env variable names

--- data/database.js
+++ data/database.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 3306,
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
 });
 
 module.exports = pool;
